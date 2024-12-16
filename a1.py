dffea66c75c15f7ab88adcda0b4231e10619351a
isha123321 <isha101112@hotmail.com>
2024-12-16 10:16:14 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,11 @@
+def decimal_to_binary(decimal_number):
+ binary_number = bin(decimal_number).replace("0b", "")
+ return binary_number
+
+
+
+decimal_number = int(input("Enter a decimal number: "))
+if binary_number = decimal_to_binary(decimal_number):
+            print("The binary representation of {decimal_number} is: {binary_number}")
+else: 
+            print("Please enter a valid integer.")
